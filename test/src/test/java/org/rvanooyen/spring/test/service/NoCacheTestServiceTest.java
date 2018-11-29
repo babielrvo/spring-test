024706da361bf6b1587391cf50dd378475ce28eb
Robin van Ooyen <r.vanooyen@babiel.com>
2018-11-29 12:56:54 +0100

    test without extended Class

--- test/src/test/java/org/rvanooyen/spring/test/service/NoCacheTestServiceTest.java
+++ test/src/test/java/org/rvanooyen/spring/test/service/NoCacheTestServiceTest.java
@@ -1,8 +1,42 @@
 package org.rvanooyen.spring.test.service;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 @ActiveProfiles("noCache")
-public class NoCacheTestServiceTest extends TestServiceTest {
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:spring/testConfig.xml"})
+public class NoCacheTestServiceTest {
 
+  @Autowired
+  @Qualifier("testService")
+  public TestService service;
+
+
+  @Before
+  public void setUp() throws Exception {
+  }
+
+
+  @Test
+  public void shouldReturnTrue() {
+    assertTrue(service.doSomething());
+  }
+
+
+  @Test
+  public void shouldGiveRightAmount() {
+    int count = service.getCount();
+    service.addOne();
+    assertEquals(count+1,service.getCount());
+  }
 }
